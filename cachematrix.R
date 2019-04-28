b31a7d497d61cbfb938cda087ec71cae17519757
abanados <48289554+abanados@users.noreply.github.com>
2019-04-28 17:17:46 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,10 @@
 ## functions do
 
 ## Write a short comment describing this function
+## makeCasheMatrix function will first initial the x as a matrix type and 
+## set m to NULL (m is the inverse of matrix)
+## 4 basic sub-functions has been defined as set, get, setmean, 
+## and getmean.  makeCasheMatrix will return a list.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -20,6 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## cacheSolve function will first check if inverse of matrix is Null
+## or not.  Since makeCacheMatrix() sets the cached inverse to NULL 
+## whenever a new vector is set into the object, if the value here is 
+## not equal to NULL, we have a valid, cached inverse and can return it 
+## to the parent environment. After validation, the function will then 
+## inverse the matrix and return the data.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
